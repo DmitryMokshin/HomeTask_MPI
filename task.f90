2f6b64a02f8b483dfa7366d13ddf34ff0ad32964
Dmitry <D3mon>
2019-05-02 03:14:07 +0300

    Коррекция оформления кода

--- task.f90
+++ task.f90
@@ -1,93 +1,90 @@
 module Task
-use :: mpi
+use::mpi
   contains
 
   subroutine GetMaxCoordinates(A,x1,y1,x2,y2)
-    implicit none
-      real(8),dimension(:,:), intent(in) :: A
-      integer(4),intent(out) :: x1,y1,x2,y2
-      integer(4) :: n,left,right,up,down,m,tr,minpos,i
-      integer(4) :: mpiErr, mpiSize, mpiRank
-      real(8),allocatable :: curcol(:),B(:,:)
-      real(8),allocatable :: local_maxsum(:),res_maxsum(:)
-      integer(4) :: local_coord(4)
-      real(8) :: cursum,ressum,maxsum
-      logical :: transpos
+   implicit none
+    real(8), dimension(:,:),intent(in) :: A
+    integer(4), intent(out) :: x1,y1,x2,y2
+    integer(4) :: n,left,right,up,down,m,tr,min_pos,i
+    integer(4) :: mpiErr,mpiSize,mpiRank,local_coord(4)
+    real(8),allocatable :: current_column(:),B(:,:)
+    real(8),allocatable :: local_max_sum_vector(:),global_max_sum(:)
+    real(8) :: current_sum,inter_sum,local_max_sum
+    logical :: transpos
 
-       call mpi_init(mpiErr)
-       call mpi_comm_size(MPI_COMM_WORLD, mpiSize, mpiErr)
-       call mpi_comm_rank(MPI_COMM_WORLD, mpiRank, mpiErr)
-       
-       m=size(A,dim=1)
-       n=size(A,dim=2)
-       transpos=.FALSE.
-        if (m<n) then
+     call mpi_init(mpiErr)
+     call mpi_comm_size(MPI_COMM_WORLD,mpiSize,mpiErr)
+     call mpi_comm_rank(MPI_COMM_WORLD,mpiRank,mpiErr)
 
+     m=size(A,dim=1)
+     n=size(A,dim=2)
+     transpos=.FALSE.
+
+         if (m<n) then
            transpos=.TRUE.
            B=transpose(A)
            m=size(B,dim=1)
            n=size(B,dim=2)
-
-        else
-
+         else
            B=A
+         endif
 
-        endif
+     allocate(current_column(m))
+     local_coord=1; local_max_sum=B(1,1)
 
-       allocate(curcol(m))
-       local_coord=1; maxsum=B(1,1)
-
-        do left=1+mpiRank,n,mpiSize
-
-           curcol=B(:,left)
+         do left=1+mpiRank,n,mpiSize
+           current_column=B(:,left)
 
              do right=left,n
 
                  if (right>left) then
-                    curcol=curcol+B(:,right)
+                   current_column=current_column+B(:,right)
                  endif
 
-                cursum=curcol(1)
-                up=1; down=1; ressum=0; minpos=0;
+               current_sum=current_column(1)
+               up=1; down=1; inter_sum=0; min_pos=0;
 
-                  do i=1, size(curcol)
-                     ressum=ressum+curcol(i)
+                 do i=1,size(current_column)
+                   inter_sum=inter_sum+current_column(i)
 
-                      if (ressum > cursum) then
-                         cursum=ressum
-                         up=minpos+1
-                         down=i
-                      endif
+                     if (inter_sum>current_sum) then
+                       current_sum=inter_sum
+                       up=min_pos+1
+                       down=i
+                     endif
 
-                      if (ressum < 0) then
-                         ressum=0 
-                         minpos=i
-                      endif
+                     if (inter_sum<0) then
+                       inter_sum=0
+                       min_pos=i
+                     endif
+                 enddo
 
-                  enddo
-               
-          if (cursum > maxsum) then
-         maxsum=cursum
-         local_coord(1)=up
-         local_coord(2)=down
-         local_coord(3)=left
-         local_coord(4)=right
-         end if
+                 if (current_sum>local_max_sum) then
+                   local_max_sum=current_sum
+                   local_coord(1)=up
+                   local_coord(2)=down
+                   local_coord(3)=left
+                   local_coord(4)=right
+                 endif
              enddo
+         enddo
 
-        enddo
-       allocate(local_maxsum(0:mpiSize-1),res_maxsum(0:mpiSize-1))
-       local_maxsum(mpiRank)=maxsum
-       call mpi_allreduce(local_maxsum,res_maxsum,mpiSize,mpi_real8,MPI_MAX,MPI_COMM_WORLD,mpiErr)
-       call mpi_bcast(local_coord,4,mpi_integer,maxloc(res_maxsum,dim=1)-1,MPI_COMM_WORLD,mpiErr)
-       x1=local_coord(1)
-       x2=local_coord(2)
-       y1=local_coord(3)
-       y2=local_coord(4)       
-       deallocate(local_maxsum,res_maxsum)
-       deallocate(curcol)
+     allocate(local_max_sum_vector(0:mpiSize-1),global_max_sum(0:mpiSize-1))
 
-        if (transpos) then
+     local_max_sum_vector(mpiRank)=local_max_sum
+     call mpi_allreduce(local_max_sum_vector,global_max_sum,mpiSize,mpi_real8,MPI_MAX,MPI_COMM_WORLD,mpiErr)
+     call mpi_bcast(local_coord,4,mpi_integer,maxloc(global_max_sum,dim=1)-1,MPI_COMM_WORLD,mpiErr)
+
+     deallocate(local_max_sum_vector,global_max_sum)
+     deallocate(current_column)
+
+     x1=local_coord(1)
+     x2=local_coord(2)
+     y1=local_coord(3)
+     y2=local_coord(4)
+
+         if (transpos) then
            tr=x1
            x1=y1
            y1=tr
@@ -95,10 +92,9 @@ use :: mpi
            tr=y2
            y2=x2
            x2=tr
+         endif
 
-        endif
-
-       call mpi_finalize(mpiErr)
+     call mpi_finalize(mpiErr)
 
   end subroutine GetMaxCoordinates
 
